a560aee9169c580f951ddd8508c6ab70cf9f3973
nhson19 <57889996+nhson19@users.noreply.github.com>
2020-11-18 14:05:28 +0700

    Update People.cpp

--- Road Crossing Game/Road Crossing Game/People.cpp
+++ Road Crossing Game/Road Crossing Game/People.cpp
@@ -1,32 +1,38 @@
 #include "People.h"
+#include "Console.h"
+
 
 People::People() {
 
 }
 
 void People::Up(int) {
-
+	if (my != 0) my--;
+	return my;
 }
 
 void People::Left(int) {
-
+	if (mx != 0) mx--;
+	return mx;
 }
 
 void People::Right(int) {
-
+	if (mx != 90) mx++;
+	return mx;
 }
 
 void People::Down(int) {
-
+	if (my != 50) my++;
+	return my;
 }
 
-bool People::isImpact(const Vehicle*&) {
-
+bool People::isImpact(const Vehicle*& a) {
+	if (mx == a->mx && my = a->my) return true;
 	return false;
 }
 
-bool People::isImpact(const Animal*&) {
-
+bool People::isImpact(const Animal*& a) {
+	if (mx == a->mx && my = a->my) return true;
 	return false;
 }
 
@@ -35,7 +41,10 @@ bool People::isFinish() {
 }
 
 bool People::isDead() {
-	return true;
+	if (this->isImpact()) {
+		this->state = false;
+		return true;
+	}
 }
 
 void People::Erase() {
